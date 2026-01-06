0d6153de4cc9284e479061796fd561a30211f19c
taimourz <taimour.rkt@gmail.com>
2026-01-06 18:46:26 +0500

    custom subdomain

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Dawn-news-rewinded-12-years/' : '/',
+  base: '/',
   plugins: [
     react({
       babel: {
